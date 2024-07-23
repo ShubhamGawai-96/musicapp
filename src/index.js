50e44115aec6326096752e58465a3002defb3e33
ShubhamGawai-96 <117394210+ShubhamGawai-96@users.noreply.github.com>
2024-07-24 03:27:13 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
 
+// Create the root element
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the App component wrapped with Router
 root.render(
   <React.StrictMode>
-    <App />
+    <Router basename={process.env.https://shubhamgawai-96.github.io/musicapp/}>
+      <App />
+    </Router>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
